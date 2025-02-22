bc2c24953f0cb97bf012598bdbb502d122086d78
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-23 00:00:05 +0900

    feat: Link 상수 생성

---
+++ app/constants/link.ts
@@ -0,0 +1,8 @@
+export const LINK = [
+  { path: '/chart', name: '차트' },
+  { path: '/Whook', name: 'Whook' },
+  { path: '/event', name: '이벤트' },
+  { path: '/news', name: '뉴스' },
+  { path: '/store', name: '스토어' },
+  { path: '/charge', name: '충전소' },
+]
