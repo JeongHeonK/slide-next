e97c92f45326dc3d690983414754326401d4027f
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-23 17:42:41 +0900

    fix: 상수 오타 수정

--- app/constants/banners.ts
+++ app/constants/banners.ts
@@ -20,14 +20,8 @@ export const BANNER = [
   {
     title: '[M COUNTDOWN] 10월 2주차 엠카 사전과제 보러가기',
     description: '2020.02.08 10:00 ~ 2020.04.08 17:00(KST)',
-    image: { name: 'image1', src: '/img/image1.jpg' },
-    isProgressing: false,
-  },
-  {
-    title: '[M COUNTDOWN] 10월 2주차 엠카 사전과제 보러가기',
-    description: '2020.02.08 10:00 ~ 2020.04.08 17:00(KST)',
     image: { name: 'image4', src: '/img/image4.jpg' },
-    isProgressing: true,
+    isProgressing: false,
   },
   {
     title: '[M COUNTDOWN] 10월 2주차 엠카 사전과제 보러가기',
