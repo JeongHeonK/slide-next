96c13e1713ddc5d43f35a83d2e9280c76b9b5e1a
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-23 16:27:48 +0900

    feat: footer 추가

--- app/(swipeablePages)/layout.tsx
+++ app/(swipeablePages)/layout.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
       <Header />
       {children}
+      <Footer />
     </>
   )
 }
